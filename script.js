f3ec4b45875c44e554c0ff64c9077370f486d026
Horakhsh Sharifi <horakhshsharifi1@gmail.com>
2023-07-31 16:45:06 +0500

    sixth commit

--- script.js
+++ script.js
@@ -9,22 +9,30 @@ const completeOrder = document.getElementById("complete-order");
 const orderCompleted = document.getElementById("ordercompleted");
 const payMaony = document.getElementById("paymentmethod");
 const yourFoodOrderPrice = document.getElementById("food-price-order");
-
+const userName = document.getElementById("user-name");
+const userNameInputField = document.getElementById("name-inp");
+const errorMessage = document.getElementById("errorMessage");
+const foodsName = document.getElementById("name-price-order");
+const cardNumInp = document.getElementById("card-num-inp");
+const cardCvvInp = document.getElementById("cvv-inp");
 const foods = [
   {
     id: "Pizza",
     description: "pepperoni, mushroom, mozzarella",
     price: 14,
+    delete: "remove",
   },
   {
     id: "Hamburger",
     description: "beef, cheese, lettuce",
     price: 12,
+    delete: "remove",
   },
   {
     id: "Beer",
     description: "grain, hops, yeast, water",
     price: 12,
+    delete: "remove",
   },
 ];
 
@@ -41,18 +49,28 @@ openYourOrder2.addEventListener("click", function () {
 openYourOrder3.addEventListener("click", function () {
   displayFoodItem(2);
 });
-
 function displayFoodItem(index) {
   yourOrder.style.display = "block";
   orderCompleted.style.display = "none";
   const foodName = document.createElement("p");
+  const removeItem = document.createElement("a");
+  removeItem.style.textDecoration = "none";
+  removeItem.style.color = "gray";
+  removeItem.style.fontSize = "13px";
+  removeItem.style.cursor = "pointer";
+  removeItem.style.marginTop = "-32.5px";
+  removeItem.style.marginLeft = "60.5px";
+  removeItem.style.display = "block";
+  removeItem.textContent = foods[index].delete;
+
   foodName.textContent = foods[index].id;
   const foodPrice = document.createElement("p");
   foodPrice.textContent = "$" + foods[index].price;
   yourFoodOrderName.appendChild(foodName);
   yourFoodOrderPrice.appendChild(foodPrice);
+  yourFoodOrderName.appendChild(removeItem);
 
-  selectedItems.push(foods[index]); // Add the selected item to the array of selected items
+  selectedItems.push(foods[index]);
 
   let totalOrderPrice = 0;
   for (let i = 0; i < selectedItems.length; i++) {
@@ -60,17 +78,38 @@ function displayFoodItem(index) {
   }
 
   yourTotalPrice.textContent = "$" + totalOrderPrice;
+  removeItem.addEventListener("click", function () {
+    selectedItems = [];
+    yourFoodOrderName.textContent = "";
+    yourFoodOrderPrice.textContent = "";
+    yourTotalPrice.textContent = "$0";
+  });
 }
 
 completeOrder.addEventListener("click", function () {
   paymentdetail.style.display = "block";
+  document.body.style.display = "grid";
+  document.body.style.justifyItems = "center";
 });
-
-payMaony.addEventListener("click", function () {
-  paymentdetail.style.display = "none";
-  yourOrder.style.display = "none";
+payMaony.addEventListener("submit", function () {
   orderCompleted.style.display = "block";
-  yourFoodOrderName.textContent = null;
-  yourFoodOrderPrice.textContent = null;
-  selectedItems = []; // Clear the selected items array when completing the order
+  userName.textContent = userNameInputField.value;
+});
+payMaony.addEventListener("submit", function (event) {
+  if ((!userNameInputField.value, !cardNumInp.value, !cardCvvInp.value)) {
+    event.preventDefault();
+    userNameInputField.style.border = "2px solid red";
+    cardNumInp.style.border = "2px solid red";
+    cardCvvInp.style.border = "2px solid red";
+  } else {
+    userNameInputField.style.border = "1px solid #bfbfbfdc";
+    cardNumInp.style.border = "1px solid #bfbfbfdc";
+    cardCvvInp.style.border = "1px solid #bfbfbfdc";
+    paymentdetail.style.display = "none";
+    yourOrder.style.display = "none";
+    document.body.style.display = null;
+    yourFoodOrderName.textContent = null;
+    yourFoodOrderPrice.textContent = null;
+    selectedItems = [];
+  }
 });
